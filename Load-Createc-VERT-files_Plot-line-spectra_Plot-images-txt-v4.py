861d6aaa01d053d6a702d102c43f9290c2f8d715
ga32xan <domenik.zimmermann@tum.de>
2017-07-04 13:34:26 +0200

    with GUI
    Added a GUI for file selection

--- Load-Createc-VERT-files_Plot-line-spectra_Plot-images-txt-v4.py
+++ Load-Createc-VERT-files_Plot-line-spectra_Plot-images-txt-v4.py
@@ -7,30 +7,31 @@
 ################################
 ### Alexander Riss	: Data loading and header parsing
 ### Mathias Pörtner	: Adding graph abilities and plotting spectral line 'maps'
-### Domenik Zimmermann	: Documentation and dependency installation instruction, version testing, debug modes 
+### Domenik Zimmermann	: Documentation and dependency checks, GUI, debug modes 
 ################################
-"""Check scalebar size => still need to get the actual size of a pixel right!"""
+"""Check scalebar size => still need to get the actual size of a pixel!"""
 """ Disable / Enable in line 369 (Topography) / line 394 (dI/dV)"""
 ################################
 ### For dependencies on Ubuntu 16.04
 ### sudo apt-get install python3 python3-matplotlib python3-numpy python3-scipy python3-pip python-gtk2-dev
 ### pip3 install matplotlib-scalebar
 ################################
-### For Windows: 
-### Install Anaconda (tested with Aanconda3) and launch it
-### Launch a qtconsole (choose within Anaconda Navigator)
-### Copy code there and execute
-################################
 # Copy the script alongside with your spectral files *.L00*.VERT into a folder
 # Edit the topographic image u want to show with gwyddion and save it as ASCII data matrix (.txt) in the same folder
-# Export options:
-#	dot decimal seperator
-#	informational header
 # Optional: Edit the dI/dV image u want and save it as ASCII data matrix (.txt) in the same folder
 #################### EDIT HERE ####################
+#This is where the series of line spectra can be found
+#spectrum_files = 'F160627.153149.L*.VERT'
 contrast_spec = 'afmhot'
+#This is the gwyddion text matrix export of the topography file, level beforehand
+#Important: Do not trim images, and record them in the same size as u did the spectra on
+#!Otherwise the positions will not match! => see non-square.txt and non-sqaure.pdf
+#data_name='F160627.152758.txt'
 contrast_topo = 'afmhot'
+#This is the gwyddion text matrix export of the topography file
+#didv_name='didv.txt'
 contrast_didv = 'afmhot'
+#with_didv = 0
 ###################################################
 # run with 'python3 Load-Createc-VERT-files_Plot-line-spectra_Plot-images-txt-v4'
 ################################
@@ -40,22 +41,22 @@ contrast_didv = 'afmhot'
 #
 #### DEBUG mode ####
 debug = 1
-debug2= 0	#adds some return values of functions ... gets a little messy
+debug2= 1	#adds some return values of functions ... gets a little messy
 ################################
 ## Terminal Color ANSI escape sequences
 class bcolors:
-    HEADER 	= '\033[95m'
-    OKBLUE 	= '\033[94m'
+    HEADER 		= '\033[95m'
+    OKBLUE 		= '\033[94m'
     WARNING 	= '\033[93m'
     OKGREEN 	= '\033[92m'
-    FAIL 	= '\033[91m'
-    ENDC 	= '\033[0m'
-    BOLD 	= '\033[1m'
+    FAIL 		= '\033[91m'
+    ENDC 		= '\033[0m'
+    BOLD 		= '\033[1m'
     UNDERLINE 	= '\033[4m'
 ################################
 
 import matplotlib
-matplotlib.use('TkAgg') 
+matplotlib.use('TkAgg') 		#uses tkinter as backend for displaying graphs
 import matplotlib.pyplot as plt		
 #import matplotlib.ticker as ticker		#Play-around with tick spacings etc. NOT implemented
 # did change to import pyplot NOT pylab since clearer namespace
@@ -67,7 +68,10 @@ import glob
 from matplotlib import gridspec
 import re
 from matplotlib_scalebar.scalebar import ScaleBar
-
+##for file choosing dialog ##
+import tkinter as tk
+from tkinter import filedialog
+##
 
 def string_simplify(str):
 	"""simplifies a string (i.e. removes replaces space for "_", and makes it lowercase"""
@@ -244,11 +248,11 @@ def save(val):
 if debug2:	print(bcolors.OKBLUE + "\nThis is funcion main()" + bcolors.ENDC)
 print(bcolors.HEADER + "#######################################################################" + bcolors.ENDC)
 print(bcolors.HEADER + "### Load-Createc-VERT-files_Plot-line-spectra_Plot-images-txt-v3 \t ###" + bcolors.ENDC)
-print(bcolors.HEADER + "###\t \t \t \t Version 3, 06/30/2017 \t \t \t ###" + bcolors.ENDC)
+print(bcolors.HEADER + "###\t \t \t Version 3, 06/30/2017 \t \t \t ###" + bcolors.ENDC)
 print(bcolors.HEADER + "### works with \t Python: 3.5.2  \t \t \t \t ###" + bcolors.ENDC)
 print(bcolors.HEADER + "### \t \t Matplotlib: 1.1.5 \t \t \t \t ###" + bcolors.ENDC)
-print(bcolors.HEADER + "### \t \t Numpy: 1.1.11 \t \t \t \t ###" + bcolors.ENDC)
-print(bcolors.HEADER + "### \t \t Scipy: 0.17.0 \t \t \t \t ###" + bcolors.ENDC)
+print(bcolors.HEADER + "### \t \t Numpy: 1.1.11 \t \t \t \t \t ###" + bcolors.ENDC)
+print(bcolors.HEADER + "### \t \t Scipy: 0.17.0 \t \t \t \t \t ###" + bcolors.ENDC)
 print(bcolors.HEADER + "#######################################################################" + bcolors.ENDC)
 
 #Load data
@@ -261,8 +265,11 @@ print(bcolors.WARNING + "#Important: Do not trim images"+ bcolors.ENDC)
 print(bcolors.WARNING + "Input something like F100101.232323.txt "+ bcolors.ENDC)
 if debug:	print ("\n...Loading Topographic information...")
 
-data_name = input(bcolors.WARNING + "Enter topography data name:"+ bcolors.ENDC)
+root = tk.Tk()
+root.withdraw()
+data_name = filedialog.askopenfilename()
 ima,imagesize=load_image(data_name)
+root.destroy()
 
 #Load data
 #dI/dV
@@ -274,13 +281,16 @@ print(bcolors.WARNING + "#Important: Do not trim images"+ bcolors.ENDC)
 print(bcolors.WARNING + "Input something like F100101.232323-dIdV.txt "+ bcolors.ENDC)
 print(bcolors.WARNING + "Leave empty if no dIdV is present"+ bcolors.ENDC)
 
-input2 = input(bcolors.WARNING + "Enter dIdV data name:"+ bcolors.ENDC)
-if input2 == '':				#checking for empty string and dont read dIdV data / configure graphics output woth with_didv flag
+root = tk.Tk()
+root.withdraw()
+didv_name = filedialog.askopenfilename()
+root.destroy()
+
+if didv_name == '':				#checking for empty string and dont read dIdV data / configure graphics output woth with_didv flag
 	if debug: print("Omitting dIdV data ...")
 	with_didv = 0
 else:
 	if debug:	print ("\n...Loading dI/dV information...")
-	didv_name = input2
 	didv,didvsize=load_image(didv_name) 
 	with_didv = 1
 
@@ -290,22 +300,22 @@ print(bcolors.WARNING + "#This are the names of your spectral *.VERT files"+ bco
 print(bcolors.WARNING + "Input something like F100101.232323.L*.VERT"+ bcolors.ENDC)
 if debug:	print ("\n...Loading Spectral information...")
 
-spectrum_files = input(bcolors.WARNING + "Enter spectrum data name:"+ bcolors.ENDC)
-
-spec=glob.glob(spectrum_files)		#creates array with spectral *.VERT filenames
-spec.sort()
+root = tk.Tk()
+root.withdraw()
+spectrum_files = filedialog.askopenfilenames()		#creates a tuple with selected spectra files files
+root.destroy()
 
 ext=[]
 matrixx=[]
 matrixy=[]
 spec_posi=[]
 count=0
-for i in spec:
+for i in spectrum_files:
 	x,y,posi=load_spec(i)
 	matrixy.append(y)
 	matrixx.append(x)
 	spec_posi.append(posi)
-	if i==spec[0] or i==spec[-1]:
+	if i==spectrum_files[0] or i==spectrum_files[-1]:
 		ext.append(posi)
 	count=count+1
 matrixx=np.array(matrixx,float)
@@ -338,6 +348,7 @@ for n,i in enumerate(matrixy):
 		if m%5==4:
 			matrixyneu[-1].append(sum(matrixy[n][m-4:m])/5)
 matrixyneu=np.array(matrixyneu,float)
+
 ###################
 # Choose contrast range
 if debug2:	print(bcolors.WARNING + "\n_Input needed_" + bcolors.ENDC)	
@@ -351,20 +362,26 @@ while ans!='o' or ans!='n':
 		break
 	elif ans=='n':
 		#find out clim topo
+		if debug2: print("... for topography")
+
 		topounten,topooben=contrast(ima,contrast_topo)
-		cons.append(topounten)
+		cons.append(topounten)							#Code hangs here
 		cons.append(topooben)
+		if debug2: print("Done for topography")
 		#find out clim didv
 
 		if with_didv:
 			didvunten,didvoben=contrast(didv,contrast_didv)
 			cons.append(didvunten)
 			cons.append(didvoben)
+			if debug2: print("Done for dIdV")
 
 		#find out clim specs along line
 		specunten,specoben=contrast(matrixyneu.T,contrast_spec)
 		cons.append(specunten)
 		cons.append(specoben)
+		if debug2: print("Done for spectra")
+
 
 		if debug: print("Saving new contrasts in file {0}".format(data_name[:-4]+'.csv'))
 		np.savetxt(data_name[:-4]+'.csv',cons,delimiter=',')
